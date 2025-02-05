07a435c0258066bdc5ceb9ac8971fa2371ed6b9c
Mattymattk14 <matthewk14@outlook.com>
2025-02-04 20:49:16 -0700

    commented out the card portion in page.js

--- app/page.js
+++ app/page.js
@@ -29,23 +29,23 @@ export default function Home() {
       fetchMonsters();
     }, []);
 
-    if (loading)
-    return (
-      <Card className="w-full max-w-2xl">
-        <CardContent className="p-6">
-          <div>Loading monsters...</div>
-        </CardContent>
-      </Card>
-    );
+  //   if (loading)
+  //   return (
+  //     <Card className="w-full max-w-2xl">
+  //       <CardContent className="p-6">
+  //         <div>Loading monsters...</div>
+  //       </CardContent>
+  //     </Card>
+  //   );
 
-  if (error)
-    return (
-      <Card className="w-full max-w-2xl">
-        <CardContent className="p-6">
-          <div className="text-red-500">Error: {error}</div>
-        </CardContent>
-      </Card>
-    );
+  // if (error)
+  //   return (
+  //     <Card className="w-full max-w-2xl">
+  //       <CardContent className="p-6">
+  //         <div className="text-red-500">Error: {error}</div>
+  //       </CardContent>
+  //     </Card>
+  //   );
 
 
   return (
