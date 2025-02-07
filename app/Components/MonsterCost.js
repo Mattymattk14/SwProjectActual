705ab865c2538c0598eb1f62f604a6c045ec9c73
Mattymattk14 <matthewk14@outlook.com>
2025-02-06 17:35:03 -0700

    fixed mapping to mostercost

--- app/Components/MonsterCost.js
+++ app/Components/MonsterCost.js
@@ -2,13 +2,7 @@ import React from 'react';
 import './MonsterCost.css'
 
 function MonsterCost(props) {
-    const elementColors = {
-        light: "white",
-        dark: "purple",
-        fire: "red",
-        water: "blue",
-        wind: "yellow",
-    };
+
 
     return (
         <div >
@@ -18,7 +12,7 @@ function MonsterCost(props) {
              width={20}
              height={25}
              viewBox='0 0 20 30'>
-                <ellipse cx={11} cy={12} rx={10} ry={11} fill={elementColors[props.monster.element.toLowerCase()] || "gray"} stroke='black' strokeWidth={2} />
+                <ellipse cx={11} cy={12} rx={10} ry={11} fill={elementColors[props.colors.toLowerCase()] || "gray"} stroke='black' strokeWidth={2} />
                 <text x={11} y={16} fontSize={15} fill='black' textAnchor='middle' fontWeight="bold" 
                       
                 // fontFamily=" 'Beleren BOld', 'Garamond', 'Georgia', serif "
