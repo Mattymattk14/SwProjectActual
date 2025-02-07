6427106b196282a24d35c1122a81ce4b5646696c
Mattymattk14 <matthewk14@outlook.com>
2025-02-06 17:30:22 -0700

    more css updates and updates to the short and long description

--- app/Components/MonsterCard.js
+++ app/Components/MonsterCard.js
@@ -17,18 +17,18 @@ function MonsterCard(props) {
                 <div className='MonsterTitleContainer'>
                 <div className='MonsterTitle'>{props.monster.name}
                 </div>
-                   <div className="MonsterCostContainer"><MonsterCost cost={props.monster.natural_stars}/></div> 
+                   <div className="MonsterCostContainer"><MonsterCost cost={props.monster.natural_stars} colors={props.monster.element}/></div> 
                 <div className='MonsterElement'></div>
                 </div>
                 <div className='MonsterImage'>
                     <img src={`https://swarfarm.com/static/herders/images/monsters/${props.monster.image_filename}`} alt={props.monster.name} />
                 </div>
-                <div className='ShortDescription'>Short Description Here</div>
+                <div className='ShortDescription'>{props.monster.bestiary_slug}</div>
                 <div className='MainDescription'>
-                    HP: {props.monster.max_lvl_hp} 
-                    ATK: {props.monster.max_lvl_attack}
-                    DEF: {props.monster.max_lvl_defense}
-                    SPD: {props.monster.speed}   
+                    <p>HP: {props.monster.max_lvl_hp}</p>
+                    <p>ATK: {props.monster.max_lvl_attack}</p>
+                    <p>DEF: {props.monster.max_lvl_defense}</p>
+                    <p>SPD: {props.monster.speed}</p>                       
                 </div>
             </div>
     );
